c39bb5f7f970124d7980e2c6509ba7c9a58f0617
marcusmoller97 <marcusssmoller@gmail.com>
2026-02-12 16:30:50 +0100

    fixed route title and fixed some ui with navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,15 +6,26 @@ import { BookForm } from './features/books/pages/book-form/book-form';
 import { MyQuotes } from './features/quotes/pages/my-quotes/my-quotes';
 import { authGuard, guestGuard } from './features/auth/guards/auth-guard';
 import { QuoteForm } from './features/quotes/pages/quote-form/quote-form';
+import { Title } from '@angular/platform-browser';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'books', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent,  canActivate: [guestGuard] },
-    { path: 'register', component: RegisterComponent, canActivate: [guestGuard] },
-    { path: 'books', component: BooksList, canActivate: [authGuard] },
-    { path: 'books/new', component: BookForm, canActivate: [authGuard] },
-    { path: 'books/:id/edit', component: BookForm, canActivate: [authGuard] },
-    { path: 'quotes', component: MyQuotes, canActivate: [authGuard] },
-    {path: 'quotes/new', component: QuoteForm, canActivate: [authGuard] },
-    { path: 'quotes/:id/edit', component: QuoteForm, canActivate: [authGuard] }
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, title: 'Logga in', canActivate: [guestGuard] },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Registrera',
+    canActivate: [guestGuard],
+  },
+  { path: 'books', component: BooksList, title: 'Böcker', canActivate: [authGuard] },
+  { path: 'books/new', component: BookForm, title: 'Ny bok', canActivate: [authGuard] },
+  { path: 'books/:id/edit', component: BookForm, title: 'Redigera bok', canActivate: [authGuard] },
+  { path: 'quotes', component: MyQuotes, title: 'Mina citat', canActivate: [authGuard] },
+  { path: 'quotes/new', component: QuoteForm, title: 'Nytt citat', canActivate: [authGuard] },
+  {
+    path: 'quotes/:id/edit',
+    component: QuoteForm,
+    title: 'Redigera citat',
+    canActivate: [authGuard],
+  },
 ];
