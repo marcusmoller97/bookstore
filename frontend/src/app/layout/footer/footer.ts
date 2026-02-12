84c0775902c8a528a7890cb1a7c9a4988762e6e4
marcusmoller97 <marcusssmoller@gmail.com>
2026-02-12 16:55:14 +0100

    added footer and som style

---
+++ frontend/src/app/layout/footer/footer.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss',
+})
+export class Footer {
+  
+
+}
